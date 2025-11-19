9dd441859dedb609f9f3190d3099777d699e088e
Mohammed Shanid <mohammedshanid888@gmail.com>
2025-11-19 07:14:14 +0000

    update the routes

--- src/features/auth/routes/AuthRoutes.jsx
+++ src/features/auth/routes/AuthRoutes.jsx
@@ -2,6 +2,12 @@ import { lazy } from 'react';
 import AuthLayout from '@/layouts/AuthLayout';
 import ErrorBoundary from '../../../shared/components/ErrorBoundary';
 import Login from '../pages/Login';
+// import { TemporaryLogin } from '../pages/TemporaryLogin';
+// import SetPassword from '../pages/SetPassword';
+// import TermsAndConditions from '../pages/TermsAndConditions';
+// import IdentityVerificationScreen from '../pages/IdentityVerification';
+// import OTPVerification from '../pages/OTPVerification';
+// import CreatePasswordScreenUI from '../pages/ResetPassword';
 
 const LoginForm = lazy(() => import('../components/forms/LoginForm'));
 // const TempLogin = lazy(() => import('../components/forms/TempLogin'));
@@ -16,30 +22,30 @@ const AuthRoutes = {
   ),
   children: [
     { path: 'login', element: <Login /> },
-      {
-        path: "/temp-login",
-        element: <TemporaryLogin/>,
-      },
-      {
-        path: "/set-password",
-        element: <SetPassword/>,
-      },
-      {
-        path: "terms",
-        element: <TermsAndConditions/>,
-      },
-      {
-        path: "identity-verification",
-        element: <IdentityVerificationScreen/>,
-      },
-      {
-        path: "otp-verification",
-        element: <OTPVerification/>,
-      },
-      {
-        path: "create-password",
-        element: <CreatePasswordScreenUI/>,
-      },
+      // {
+      //   path: "/temp-login",
+      //   element: <TemporaryLogin/>,
+      // },
+      // {
+      //   path: "/set-password",
+      //   element: <SetPassword/>,
+      // },
+      // {
+      //   path: "terms",
+      //   element: <TermsAndConditions/>,
+      // },
+      // {
+      //   path: "identity-verification",
+      //   element: <IdentityVerificationScreen/>,
+      // },
+      // {
+      //   path: "otp-verification",
+      //   element: <OTPVerification/>,
+      // },
+      // {
+      //   path: "create-password",
+      //   element: <CreatePasswordScreenUI/>,
+      // },
     // { path: 'temp-login', element: <TempLogin /> },
     // { path: 'reset-password', element: <ResetPassword /> },
   ],
