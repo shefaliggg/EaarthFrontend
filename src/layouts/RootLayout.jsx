37b02cf8757759e2990dbccc02b9ab006abafd17
Muhamed Razik <muhammedrazik987@gmail.com>
2025-11-26 22:47:03 +0530

    feat: Refactor QR login UI and improve auth handling in RootLayout

--- src/layouts/RootLayout.jsx
+++ src/layouts/RootLayout.jsx
@@ -1,19 +1,30 @@
-import { Outlet, useLocation } from "react-router-dom";
+import { Outlet, useLocation, Navigate } from "react-router-dom";
 import { useAuth } from "@/features/auth/context/AuthContext";
 import LoadingScreen from "@/shared/components/LoadingScreen";
 import { toast } from "sonner";
-import LoginPage from "../features/auth/pages/Login";
 import { useEffect } from "react";
+import { isDevelopment } from "../features/auth/config/axiosConfig";
 
 export default function RootLayout() {
   const { initialLoading, user } = useAuth();
-  const {pathname} = useLocation();
+  const { pathname } = useLocation();
+
   useEffect(() => {
-      if (!user && !initialLoading && !pathname.startsWith("/auth")) {
-        toast.error("User not authenticated");
-        return <LoginPage />;
+    if (!user && !initialLoading && !pathname.startsWith("/auth")) {
+      if (isDevelopment) {
+        toast.warning("Dev Mode:: User not authenticated.", {
+          description:
+            "Please ensure you are logged in to access all features and avoid null errors.",
+        });
+      } else {
+        toast.error("User not authenticated", {
+          description: "Redirecting to login page...",
+        });
+        return <Navigate to="/auth/login" />;
+      }
     }
   }, [initialLoading, user, pathname]);
+
   if (initialLoading) return <LoadingScreen />;
 
   return <Outlet />;
