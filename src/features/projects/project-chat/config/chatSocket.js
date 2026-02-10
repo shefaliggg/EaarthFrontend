faa1fbe06848984f72c0564130d2e9a79205eb0a
Muhamed Razik <muhamedrazik061@gmail.com>
2026-02-10 07:40:09 +0000

    fix: import socketConfig and add console logs for chatSocket events

--- src/features/projects/project-chat/config/chatSocket.js
+++ src/features/projects/project-chat/config/chatSocket.js
@@ -1,5 +1,6 @@
+import { io } from "socket.io-client";
 import { store } from "../../../../app/store";
-import { socketBaseURL } from "../../../../shared/config/socketConfig";
+import { socketBaseURL, socketConfig } from "../../../../shared/config/socketConfig";
 
 let chatSocket = null;
 
@@ -29,36 +30,44 @@ export const initChatSocket = (userId) => {
   // ---- MESSAGE EVENTS ----
   chatSocket.on("message:new", ({ conversationId, message }) => {
     // store.dispatch(messageReceived({ conversationId, message }));
+    console.log("New message received:", { conversationId, message });
   });
 
   chatSocket.on("message:edited", (payload) => {
     // store.dispatch(messageEdited(payload));
+    console.log("Message edited:", payload);
   });
 
   chatSocket.on("message:deleted", (payload) => {
     // store.dispatch(messageDeleted(payload));
+    console.log("Message deleted:", payload);
   });
 
   // ---- READ RECEIPTS ----
   chatSocket.on("conversation:read", (payload) => {
     // store.dispatch(conversationRead(payload));
+    console.log("Conversation read:", payload);
   });
 
   // ---- TYPING ----
   chatSocket.on("typing:start", (payload) => {
     // store.dispatch(typingStarted(payload));
+    console.log("Typing started:", payload);
   });
 
   chatSocket.on("typing:stop", (payload) => {
     // store.dispatch(typingStopped(payload));
+    console.log("Typing stopped:", payload);
   });
 
   // ---- PRESENCE ----
   chatSocket.on("user:online", (userId) => {
     // store.dispatch(userOnline(userId));
+    console.log("User online:", userId);
   });
 
   chatSocket.on("user:offline", (userId) => {
     // store.dispatch(userOffline(userId));
+    console.log("User offline:", userId);
   });
 };
